504f720c8b3626a07d19de3db4581040a808e81b
Anurag059 <anuragtripathi059@gmail.com>
2025-09-16 19:54:19 +0530

    Time: 7 ms (7.55%), Space: 42 MB (13.92%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,13 +1,29 @@
 class Solution {
     public String toLowerCase(String s) {
-        //return  s.toLowerCase();
-        char[] c=s.toCharArray();
-        for(int i=0;i<c.length;i++){
-            if(c[i]>=65 && c[i]<=90){
-                c[i]=(char)(c[i]+32);
+
+        //==============Solution1===============================       
+        // return s.toLowerCase();
+
+
+        //================Solution2========convert string to array=================        
+        // char[] c = s.toCharArray();
+        // for (int i = 0; i < c.length; i++) {
+        //     if (c[i] >= 'A' && c[i] <= 'Z') {
+        //         c[i] = (char)(c[i] + 32);
+        //     }
+        // }
+        // return String.valueOf(c);
+
+
+        //=================Solution3===========without using array=============
+        String result = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) >= 'A' && s.charAt(i) <= 'Z') {
+                result += (char)(s.charAt(i) + 32);
+            } else {
+                result += s.charAt(i);
             }
         }
-        return String.valueOf(c);
-
+        return result;
     }
 }
