d9c9ec36bdcb9acc4ce8112ad3d760237c0127b5
Anurag059 <anuragtripathi059@gmail.com>
2025-09-17 11:17:42 +0530

    Time: 13 ms (88.57%), Space: 58.1 MB (78.77%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -2,17 +2,30 @@ import java.util.Arrays;
 
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        // Sort the array
-        Arrays.sort(nums);
+        // Sort kro array ko
+        // Arrays.sort(nums);
         
-        // Check adjacent elements for duplicates
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                return true; // Duplicate found
+        // for(int i=0;i<nums.length-1;i++){
+        //     for (int j=i+1;j<nums.length;j++){
+        //         if(nums[i]==nums[j]){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+        
+        // arraylist ka use nhi krenge kyunki time limit exceed aayegi isliye hashset ka use krenge arraylist ki jagh p
+
+        //ArrayList<Integer> list = new ArrayList<>();
+        HashSet<Integer> list = new HashSet<>();
+
+        for (int num : nums) {
+            if (list.contains(num)) { 
+                return true;  // duplicate found
             }
+            list.add(num);
         }
-        
-        // No duplicates found
-        return false;
-    }
+        return false;  // no duplicates
+       
+    }    
 }
