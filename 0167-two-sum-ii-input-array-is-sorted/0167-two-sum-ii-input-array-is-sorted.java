a43d861fcca5e2c68362af0a00583d868d3838f9
Anurag059 <anuragtripathi059@gmail.com>
2025-09-19 19:18:39 +0530

    Time: 2 ms (92.58%), Space: 47.1 MB (67.26%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -2,22 +2,21 @@ class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int left = 0;
         int right = numbers.length - 1;
-        int[] ans = new int[2];
+        
 
         while (left < right) {
             int sum = numbers[left] + numbers[right];
-
-            if (sum == target) {
-                ans[0] = left + 1;  // convert to 1-based index
-                ans[1] = right + 1;
-                return ans;
-            } else if (sum < target) {
-                left++;  // need a bigger sum
-            } else {
-                right--; // need a smaller sum
+            if (sum >target) {
+                right--;  // need a bigger sum
+            } else if (sum<target){
+                left++;
+            } else{
+                break;
             }
+
+            
         }
 
-        return ans;  // Just to satisfy return type, won't reach here as per constraints
+        return new int[] {left+1,right+1};  // Just to satisfy return type, won't reach here as per constraints
     }
 }
