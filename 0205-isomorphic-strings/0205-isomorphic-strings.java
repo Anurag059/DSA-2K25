e91232f1280406d3b10462775e37137e8e9d1940
Anurag059 <anuragtripathi059@gmail.com>
2026-02-19 20:04:16 +0530

    Time: 20 ms (16.39%), Space: 43.9 MB (58.4%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -19,31 +19,32 @@ class Solution {
 
 
         //---------------------------------------------
-          if (s.length() != t.length()) return false;
-        
-        HashMap<Character, Character> map1 = new HashMap<>();
-        HashMap<Character, Character> map2 = new HashMap<>();
-        
-        for (int i = 0; i < s.length(); i++) {
-            
-            char c1 = s.charAt(i);
-            char c2 = t.charAt(i);
-            
-            if (map1.containsKey(c1)) {
-                if (map1.get(c1) != c2)
+        if(s.length()!=t.length()) return false;
+
+        HashMap<Character,Character> map1=new HashMap<>();
+        HashMap<Character,Character> map2=new HashMap<>();
+
+        for(int i=0;i<s.length();i++ ){
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+            if(map1.containsKey(ch1)){
+                if(map1.get(ch1)!=ch2){
                     return false;
-            } else {
-                map1.put(c1, c2);
+                }
             }
-            
-            if (map2.containsKey(c2)) {
-                if (map2.get(c2) != c1)
+            else{
+                map1.put(ch1,ch2);
+            }
+
+
+            if(map2.containsKey(ch2)){
+                if(map2.get(ch2)!=ch1){
                     return false;
-            } else {
-                map2.put(c2, c1);
+                }
+            }else{
+                map2.put(ch2,ch1);
             }
         }
-        
         return true;
 
     }
